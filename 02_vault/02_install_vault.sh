eed3a8df5146b8805c5e39c1f56fbd3b70ea531e
Yama <yama.samoila@nordnet.se>
2024-11-15 14:47:18 +0100

    simplify vault installation

--- 02_vault/02_install_vault.sh
+++ 02_vault/02_install_vault.sh
@@ -3,7 +3,4 @@
 # bash strict mode
 set -euxo pipefail
 
-kubectl apply -f 02_vault/01_serviceaccount.yaml
-kubectl apply -f 02_vault/02_clusterrolebinding.yaml
-kubectl apply -f 02_vault/03_rbac.yaml
-kubectl apply -f 02_vault/04_vault.yaml
+kubectl apply -f 02_vault/
