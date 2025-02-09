88b9b9f221abe5c6f30d339db167d6aeba81a1eb
Yama <yama.samoila@nordnet.se>
2025-02-09 20:41:29 +0100

    wip

--- flux-apps/platform/04_linkerd/install_linkerd.sh
+++ flux-apps/platform/04_linkerd/install_linkerd.sh
@@ -6,6 +6,7 @@ set -euxo pipefail
 # Install Linkerd CLI
 brew install linkerd step openssl
 
+# Certs: https://linkerd.io/2-edge/tasks/generate-certificates/
 # Generate a certificate authority (valid for 30y)
 step certificate create root.linkerd.cluster.local linkerd_ca.crt linkerd_ca.key \
 --profile root-ca \
@@ -24,4 +25,20 @@ step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
 # Preflight Check
 linkerd check --pre
 
-# Install Linkerd
+# Set up Linkerd Helm repo
+helm repo add linkerd-edge https://helm.linkerd.io/edge
+
+# Install Linkerd CRDs
+helm install linkerd-crds linkerd-edge/linkerd-crds -n sys-linkerd
+
+# Install Linkerd Control Plane
+helm install linkerd-control-plane \
+  -n sys-linkerd \
+  --set-file identityTrustAnchorsPEM=ca.crt \
+  --set-file identity.issuer.tls.crtPEM=issuer.crt \
+  --set-file identity.issuer.tls.keyPEM=issuer.key \
+  --set proxy.nativeSidecar=true \
+  linkerd-edge/linkerd-control-plane
+
+# Todo: figure out how to push the ca.crt and issuer.crt and issuer.key into secrets and load them in the helm chart
+# Exmaple: https://chatgpt.com/c/67a8fc2d-2b54-8000-8009-3429c5173810
