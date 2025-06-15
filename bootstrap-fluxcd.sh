fcc9b80952fb1366079552c234542e69a562bba7
Yama <yama.samoila@nordnet.se>
2025-06-15 12:48:57 +0300

    add nginx ingress (#15)

--- bootstrap-fluxcd.sh
+++ bootstrap-fluxcd.sh
@@ -40,6 +40,10 @@ images=(
   "ghcr.io/fluxcd/flux-cli:v2.4.0"
   "ghcr.io/fluxcd/kustomize-controller:v1.4.0"
   "docker.io/bitnami/sealed-secrets-controller:0.28.0"
+  "registry.k8s.io/ingress-nginx/kube-webhook-certgen:v1.4.0@sha256:44d1d0e9f19c63f58b380c5fddaca7cf22c7cee564adeff365225a5df5ef3334"
+  "registry.k8s.io/ingress-nginx/controller:1.12.0@sha256:42b3f0e5d0846876b1791cd3afeb5f1cbbe4259d6f35651dcc1b5c980925379c"
+  "docker.l5d.io/buoyantio/emojivoto-web:v11"
+  "docker.l5d.io/buoyantio/emojivoto-voting-svc:v11"
   "ghcr.io/gimlet-io/capacitor:v0.4.8"
   "ghcr.io/weaveworks/wego-app:v0.38.0"
 )
@@ -56,15 +60,13 @@ wait
 # Install sealed secrets controller (needed for FluxCD's secret containing the github API key)
 # More sophisticated config will be applied once FluxCD takes over (see flux-apps/platform/02_sealed_secrets/helm-release.yaml)
 # Todo: renovate the versions below as well as the ones in the helm-release.yaml
-helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
-helm repo update sealed-secrets
+helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets > /dev/null
+helm repo update sealed-secrets > /dev/null
 # "sealed-secrets" must match the metadata.name in k8s/platform-charts/02_sealed_secrets/helm-release.yaml
 # to ensure that the Sealed Secrets helm-chart is overridden by the flux-managed one and sealed-secrets is thus managed by flux in the end.
 helm install sealed-secrets sealed-secrets/sealed-secrets \
 --namespace sys-sealed-secrets --create-namespace \
---version 2.17.1 \
---set-string "image.registry=docker.io" \
---set-string image.repository="bitnami/sealed-secrets-controller"
+--version 2.17.1  > /dev/null
 
 # Wait for sealed-secrets-controller to be ready (we need the CRDs to be installed at least)
 set +x
@@ -78,11 +80,15 @@ if [ $i -eq 60 ]; then
   exit 1;
 fi;
 
+sleep 10;
+
+
 # Todo: backup the sealed-secrets-controller's private key and public key on disk,
 #       and find a way for it to never delete it in the cluster no matter what, even if the HelmRelease is deleted.
 #       And research and document how to recover the cluster's private key from a backup.
 
 
+echo "Preparing Sealed Secrets for FluxCD and Weave GitOps..."
 # Create Sealed Secret (replace GITHUB token with yours from the environment)
 sed "s/GITHUB_API_KEY/$GITHUB_API_KEY/g" ./bootstrap-github-api-secret-template.yaml \
     | kubeseal --controller-namespace sys-sealed-secrets --controller-name sealed-secrets --format yaml \
@@ -102,8 +108,8 @@ sed "s/WEAVE_ADMIN_PASSWORD/$ESCAPED_HASH/g" ./bootstrap-weave-admin-secret-temp
 # https://artifacthub.io/packages/helm/fluxcd-community/flux2
 # More sophisticated config will be applied automatically, once FluxCD takes over (see flux-apps/platform/01_fluxcd/helm-release.yaml)
 # Todo: renovate the versions below as well as the ones in the helm-release.yaml
-helm repo add fluxcd https://fluxcd-community.github.io/helm-charts
-helm repo update fluxcd
+helm repo add fluxcd https://fluxcd-community.github.io/helm-charts > /dev/null
+helm repo update fluxcd > /dev/null
 # "fluxcd" must match the spec.releaseName in k8s/platform-charts/01_fluxcd/helm-release.yaml
 # to ensure that the Flux helm-chart is overridden by the flux-managed one and flux is thus managed by itself in the end.
 helm install fluxcd fluxcd/flux2 \
@@ -111,16 +117,15 @@ helm install fluxcd fluxcd/flux2 \
 --version 2.14.1 \
 --set imageReflectionController.create=false \
 --set imageAutomationController.create=false \
---set notificationController.create=false
+--set notificationController.create=false > /dev/null
 
-kubectl apply -f ./k8s/platform-charts/01_fluxcd/templates/gh-api-key-sealedsecret.yaml
-kubectl apply -f ./k8s/platform-charts/01_fluxcd/templates/git-repo.yaml
+kubectl apply -f ./k8s/platform-charts/01_fluxcd/templates/gh-api-key-sealedsecret.yaml > /dev/null
+kubectl apply -f ./k8s/platform-charts/01_fluxcd/templates/git-repo.yaml > /dev/null
 
 set +x
 
 echo "Now you need to git-commit and push all changes (including the sealed secrets) to your git repository."
 echo "CAUTION: the branch you want to work on must be specified in /k8s/platform-charts/01_fluxcd/templates/git-repo.yaml"
-echo "CAUTION2: you need to change it back to 'main' before you merge your branch to 'main'!"
 echo "Configured branch in /k8s/platform-charts/01_fluxcd/templates/git-repo.yaml:"
 cat ./k8s/platform-charts/01_fluxcd/templates/git-repo.yaml | grep branch:
 
--- minikube/setup_minikube.sh
+++ minikube/setup_minikube.sh
@@ -3,18 +3,21 @@
 # bash strict mode
 set -euxo pipefail
 
-# install minikube via brew if not installed
-brew install minikube || true
+# install minikube + vfit via brew if not installed
+brew install minikube vfkit || true
+minikube config set driver vfkit
 
 # start minikube
 minikube unpause || minikube start \
 --addons=metrics-server \
 --cpus=8 \
---disk-size=350g \
---delete-on-failure=true \
---driver=docker \
---kubernetes-version=v1.31.4 \
 --memory=8192 \
+--disk-size=150g \
+--delete-on-failure=true \
+--driver=vfkit \
+--kubernetes-version=v1.31.4 \
+--ports=30080:30080 \
+--ports=30443:30443 \
 --subnet=172.17.128.0/17
 #--addons=csi-hostpath-driver \
 #--addons=volumesnapshots \
