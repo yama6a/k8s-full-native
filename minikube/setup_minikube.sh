73fdf616b68fdffd0a1477ed7a45934c40e9c2b5
Yama <yama.samoila@nordnet.se>
2025-06-22 00:29:17 +0200

    wip

--- minikube/setup_minikube.sh
+++ minikube/setup_minikube.sh
@@ -7,9 +7,12 @@ set -euxo pipefail
 brew install minikube vfkit || true
 minikube config set driver vfkit
 
-# start minikube
+# start minikube (addons csi-hostpath-driver, volumesnapshots are needed for CNPG)
+# todo: When trying this on a raspi cluster, need to isntall csi-hostpath-driver and volumesnapshots manually
 minikube unpause || minikube start \
 --addons=metrics-server \
+--addons=csi-hostpath-driver \
+--addons=volumesnapshots \
 --cpus=8 \
 --memory=8192 \
 --disk-size=150g \
@@ -19,5 +22,3 @@ minikube unpause || minikube start \
 --ports=30080:30080 \
 --ports=30443:30443 \
 --subnet=172.17.128.0/17
-#--addons=csi-hostpath-driver \
-#--addons=volumesnapshots \
